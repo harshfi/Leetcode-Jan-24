c75e83e6fa76dac0466d13e05fb5abe2deb463aa
harshfi <86000141+harshfi@users.noreply.github.com>
2024-01-02 11:54:11 +0530

    Time: 3 ms (77.12%) | Memory: 44.9 MB (14.62%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,27 +1,23 @@
 class Solution {
-    List<List<Integer>> list=new ArrayList<>();
-    public List<List<Integer>> findMatrix(int[] nums) {
-        list=new ArrayList<>();
-        boolean[] check= new boolean[nums.length];
-        int count=0;
-        while(count<nums.length){
-          List<Integer> l =  make(nums,new ArrayList<>(), check );
-          list.add(new ArrayList<>(l));
-          count=count+l.size();
+    public List<List<Integer>> findMatrix(int[] num) {
+        List <Integer> nums = new ArrayList<>();
+        for(int i=0;i<num.length;i++){
+            nums.add(num[i]);
+        }
+
+        List <List<Integer>> list = new ArrayList<>();
+        while(nums.size() > 0){
+            List <Integer> l2 = new ArrayList<>();
+            int i = 0;
+            while(i < nums.size()){
+                if(!l2.contains(nums.get(i))){
+                    l2.add(nums.get(i));
+                    nums.remove(i);
+                }
+                else{i++;}
+            }
+            list.add(l2);
         }
         return list;
-       
-        
-    }
-    public List<Integer> make(int []nums ,List<Integer> l,boolean []check ){
-       
-       for(int i=0;i<nums.length;i++){
-           if(check[i]==false && ! l.contains(nums[i]) ){
-               l.add(nums[i]);
-               check[i]=true;
-           }
-       }
-       return l;
-
     }
 }
