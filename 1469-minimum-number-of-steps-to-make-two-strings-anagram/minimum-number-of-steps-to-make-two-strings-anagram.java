a6d665a965ccdae191500f7332bd5f1be223c28f
harshfi <86000141+harshfi@users.noreply.github.com>
2024-01-13 15:24:09 +0530

    Time: 9 ms (63.21%) | Memory: 45.2 MB (24.18%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,27 +1,18 @@
 class Solution {
     public int minSteps(String s, String t) {
-        Map<Character,Integer> mapA = new HashMap<>();
-        Map<Character,Integer> mapB = new HashMap<>();
-        for(int i=0;i<s.length();i++){
-           
-           char ch1= s.charAt(i);
-           char ch2=t.charAt(i);
-           mapA.put(ch1,mapA.getOrDefault(ch1,0)+1);
-           mapB.put(ch2,mapB.getOrDefault(ch2,0)+1);
+        int[] charFreq = new int[26];
 
+        for(int idx=0; idx < s.length(); idx++) {
+            charFreq[s.charAt(idx)-'a']++;
+            charFreq[t.charAt(idx)-'a']--;
         }
-           int sum=0;
-        for(Map.Entry<Character,Integer> entry : mapA.entrySet()){
 
-            if(mapB.containsKey(entry.getKey())&& mapB.get(entry.getKey())<=entry.getValue() ){
-               sum=sum+(entry.getValue() - mapB.get(entry.getKey()));
+        int minSteps = 0;
 
-            }
-            else if(!mapB.containsKey(entry.getKey())) {
-                sum=sum+entry.getValue();
-            }
+        for(int idx=0; idx < 26; idx++) {
+            minSteps += Math.max(0, charFreq[idx]);
         }
-        return sum;
-        
+
+        return minSteps;
     }
 }
